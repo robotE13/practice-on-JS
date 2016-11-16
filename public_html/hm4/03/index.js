fdf573ef14be68776243b45f4cc09c26ce14b666
robotE13 <wolfstrace@gmail.com>
2016-11-16 22:27:08 +0300

    init

---
+++ public_html/hm4/03/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+ *
+ * @param {Element} element
+ * @returns {undefined}
+ */
+function deleteTextNodes(element)
+{
+    var index = 0, children = element.childNodes;
+
+    while(index < children.length)
+    {
+        if(children[index].nodeType === Node.TEXT_NODE)
+        {
+            children[index].remove();
+        }else if(children[index].nodeType === Node.ELEMENT_NODE){
+            deleteTextNodes(children[index++]);
+        }else{
+            index++;
+        }
+    }
+}
+
+/**
+ *
+ * @param {Document} $
+ * @returns {undefined}
+ */
+(function($){
+    $.addEventListener("DOMContentLoaded",function(){
+        var container = $.getElementById('main-container');
+
+        $.getElementById("clever-btn").addEventListener("click",function(){
+            console.info(container.textContent);
+            deleteTextNodes(container);
+            console.info(container.textContent);
+        });
+    });
+})(document);
