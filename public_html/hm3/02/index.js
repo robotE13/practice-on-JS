7e9a041145778bd19855d4c47b69976e8175c2dc
robotE13 <wolfstrace@gmail.com>
2016-11-19 14:45:19 +0300

    add сначала сравнение классов передаваемых объектов

--- public_html/hm3/02/index.js
+++ public_html/hm3/02/index.js
@@ -18,11 +18,17 @@ function deepEqual(variable1, variable2)
         return variable1 === variable2;
     }
 
-    iterator = Object.keys(variable1);
-    if(variable1 instanceof Date || variable2 instanceof Date)//здесь именно || иначе Date и пустой Array не попадут вообще в сравнение и вернется true
+    if(variable1.constructor().toString() !== variable2.constructor().toString())
+    {
+        return false;
+    }
+
+    if(variable1 instanceof Date && variable2 instanceof Date)
     {
         return variable1.valueOf() === variable2.valueOf();
     }
+
+    iterator = Object.keys(variable1);
     if(iterator.length !== Object.keys(variable2).length)
     {
         return false;
@@ -66,13 +72,29 @@ var objA = {
     prop7: null
 };
 
+var objA1 = {
+    prop1: 'value1',
+    prop2: 'value2',
+    prop3: 'value3',
+    prop4: {
+        subProp1: 'sub value1',
+        subProp2: {
+            subSubProp1: 'sub sub value1',
+            subSubProp2: [1, 2, {prop2: 1, prop: 2}, 4, 5]
+        }
+    },
+    prop5: 1000,
+    prop6: new Date(2016, 2, 10),
+    prop7: null
+};
+
 var objB = {
     prop5: 1000,
     prop3: 'value3',
     prop7: null,
     prop1: 'value1',
     prop2: 'value2',
-    prop6: new Date('2016/03/10'),
+    prop6: [],
     prop4: {
         subProp2: {
             subSubProp1: 'sub sub value1',
@@ -116,7 +138,8 @@ var arrA = [1,2,3,4,5,6];
 var arrB = [1,2,3,4,5,6,7];
 var arrC = [1,3,2,4,5,6];
 
-console.log(deepEqual(objA,objB));   //true
+console.log(deepEqual(objA,objA1));   //true
+console.log(deepEqual(objA,objB));   //prop6 false
 console.log(deepEqual(objA,objC));   //false
 console.log(deepEqual(objA,objD));   // prop6 false
 console.log(deepEqual(new Date(2016,2,10),new Date('2016/3/10'))); //true
@@ -127,3 +150,4 @@ console.log(deepEqual('12','12'));   //true
 console.log(deepEqual(arrA,null));   //false
 console.log(deepEqual(null,arrA));   //false
 console.log(deepEqual(null,null));   //true
+console.log(deepEqual(false,undefined));   //false
