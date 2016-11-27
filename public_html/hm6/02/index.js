6f3ab93bc4fa9bbb977c84312d25487f6e0516fe
robotE13 <wolfstrace@gmail.com>
2016-11-27 21:43:01 +0300

    add filter

--- public_html/hm6/02/index.js
+++ public_html/hm6/02/index.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param {String} url
+ * @returns {Promise}
+ */
 function getJSON(url) {
     return new Promise(function (success,error){
         var ajax = new XMLHttpRequest();
@@ -8,19 +13,6 @@ function getJSON(url) {
         });
         ajax.send();
     });
-}
-
-/**
- *
- * @param {String} textContent
- * @param {Element} container
- * @returns {undefined}
- */
-function addData(textContent,container)
-{
-    let li = document.createElement('li');
-    li.textContent = textContent;
-    container.appendChild(li);
 }
 
 /**
@@ -65,11 +57,121 @@ function getSequence(length) {
 }
 
 document.addEventListener('DOMContentLoaded',function (){
+    var filter = {
+        /**
+         * @type Element
+         */
+        filterInput:null,
+        /**
+         * @type Array
+         */
+        allData:[],
+        /**
+         *
+         * @param {Element} filterInput
+         * @param {Array} data
+         * @returns {undefined}
+         */
+        init:function (filterInput,data)
+        {
+            var filter = this;
+            this.filterInput = filterInput;
+            this.allData = data;
+            listManager.displayData(this.allData);
+
+            this.filterInput.addEventListener('input',function () {
+                filter.trigger().then(function () {
+                    listManager.displayData(filter.apply());
+                });
+            });
+        },
+
+        /**
+         *
+         * @returns {Array}
+         */
+        apply:function ()
+        {
+            let filterValue = this.filterInput.value;
+            return this.allData.filter(item => item.indexOf(filterValue) !== -1);
+        },
+        /**
+         *
+         * @returns {Promise}
+         */
+        trigger:function ()
+        {
+            var filter = this;
+            return new Promise(function (resolve,reject){
+                setTimeout(function () {
+                    if(filter.filterInput.value.length > 2 || filter.filterInput.value.length === 0)
+                    {
+                        resolve();
+                    }else{
+                        reject();
+                    }
+                },2500);
+                filter.filterInput.addEventListener('input',function () {
+                    reject();
+                });
+            });
+        }
+    },
+    listManager = {
+        /**
+         * @type Element
+         */
+        container:document.getElementById('out'),
+        /**
+         *
+         * @param {Array} data
+         * @returns {undefined}
+         */
+        displayData : function(data)
+        {
+            let before;
+
+            this.removeOld(data);
+            before = this.container.firstElementChild;
+
+            for (let index = 0; index < data.length; index++)
+            {
+                if(before && data[index] === before.textContent)
+                {
+                    before = before.nextElementSibling;
+                }else{
+                    this.addListElement(data[index],before);
+                }
+            }
+        },
+        removeOld : function(data)
+        {
+            for(let index = 0; index < this.container.children.length;index++){
+                let element = this.container.children[index];
+                if(data.indexOf(element.textContent) === -1)
+                {
+                    this.container.removeChild(element);
+                    index--;
+                }
+            }
+        },
+
+        /**
+         *
+         * @param {String} textContent
+         * @param {Element} before description
+         * @returns {undefined}
+         */
+        addListElement:function (textContent,before = null)
+        {
+            let li = document.createElement('li');
+            li.textContent = textContent;
+            this.container.insertBefore(li,before);
+        }
+    };
+
     getJSON('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
         .then(function (result) {
-            let container = document.getElementById('out');
-            for (let city of sort(result.response)) {
-                addData(city.name,container);
-            }
+            filter.init(document.getElementById('filter'),sort(result.response).map(item=>item.name));
         });
 });
